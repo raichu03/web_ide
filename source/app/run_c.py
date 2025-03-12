445fda983dc96d88058d4c59b371cb760eb18c4a
ansan <anshanpool@gmail.com>
2025-03-12 23:59:03 +0545

    added code to run c

--- source/app/run_c.py
+++ source/app/run_c.py
@@ -6,20 +6,32 @@ def execute(file_path: str, timeout: int = 10):
     """
     Executes the python code and returns the output
     """
+    
+    executable_path = file_path.replace('.c', '')
     try:
-        # Run the Python code using subprocess
-        result = subprocess.run(
-            ['python', file_path],
+        # Run the C code using subprocess
+        compile_result = subprocess.run(
+            ['gcc', file_path, '-o', executable_path],
             text=True,
             capture_output=True,
             timeout=timeout,
             check=True
         )
         
+        result = subprocess.run(
+            [f'./{executable_path}'],
+            text=True,
+            capture_output=True,
+            timeout=timeout,
+            check=True
+        )
+        
+        os.remove(executable_path)
         #Capture standard output and standard error
         stdout = result.stdout
         stderr = result.stderr
         return_code = result.returncode
+        
     except subprocess.TimeoutExpired:
         # Handle timeout
         stdout = ""
@@ -36,6 +48,15 @@ def execute(file_path: str, timeout: int = 10):
         stderr = str(e)
         return_code = -1
     
-    stderr = re.sub(r'File ".*", ', '', stderr).strip()
-    os.remove(file_path)
-    return stdout, stderr, return_code
+    file_pattern = re.escape(file_path) + r":"
+    stderr = re.sub(file_pattern, '', stderr).strip()
+    # os.remove(file_path)
+    return stdout, stderr, return_code  
+
+if __name__ == "__main__":
+    file_path = "../temp/test.c"
+    stdout, stderr, return_code = execute(file_path)
+    print(f"stdout {stdout}")
+    print(f"stderr {stderr}")
+    print(f"return_code {return_code}")
+    
